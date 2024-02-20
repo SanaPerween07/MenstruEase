727033e44036697c9624e4a0f4c19fff2c048730
Sana Perween <sanahmad15046@gmail.com>
2024-02-20 18:22:30 +0530

    changed the footer, added tailwindcss to it

--- components/Dashbody.tsx
+++ components/Dashbody.tsx
@@ -15,8 +15,8 @@ const Dashbody = () => {
 				Take charge of your menstrual cycle with our all-in-one
 				platform. Get period tracking, sustainable products, expert
 				consultations, and more to empower your reproductive health.
-			</p><br></br>
-			<Button className="btn btn-primary">Get Strated</Button>
+			</p>
+			<Button className="btn btn-primary" variant='default' size='sm'>Get Strated</Button>
 		</div>
 	</div>
     )
